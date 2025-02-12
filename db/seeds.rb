d67fa7dfb2d3dc5fd8a655806d16a4c239f926d8
Marcus Meyer <marcusmeyer90@hotmail.fr>
2025-02-12 15:43:27 +0100

    Keep Going

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Destroying to rebuilt..."
+Flat.destroy_all
+
+puts "Creating Light Flat..."
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+puts "Creating Peace Flat..."
+Flat.create!(
+  name: 'Peace Flat',
+  address: '126th Street Manhattan NY',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+puts "Creating Easy Flat..."
+Flat.create!(
+  name: 'Easy Flat',
+  address: '130th Street Brooklyn NY',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 90,
+  number_of_guests: 7
+)
+puts "Charmant Appart..."
+Flat.create!(
+  name: 'Charmant Appart',
+  address: '75006 Paris',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 120,
+  number_of_guests: 4
+)
